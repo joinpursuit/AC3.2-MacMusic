f6f2ff4ceb7854cca82fd4357bc3e2778466e598
Marcel324 <mchaucer324@gmail.com>
2016-11-09 16:02:00 -0500

    Made A Artists Class for Browse Tab

--- SpotifySearch/Artists.swift
+++ SpotifySearch/Artists.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class Artists {
+    
+    let artistID: String
+    let image: String
+    let artistName: String
+    
+    init(artistID: String, image: String, artistName: String) {
+        self.artistID = artistID
+        self.image = image
+        self.artistName = artistName
+    }
+    
+    static func getArtists(from data: Data) -> [Artists]? {
+        var artistInfo = [Artists]()
+        do {
+            let artistJSONData: Any = try? JSONSerialization.jsonObject(with: data, options: [])
+            
+            guard let artistsCasted = artistJSONData as? [String : Any],
+            let artistArr = artistsCasted["artists"] as? [[String : Any]] else {return nil}
+            
+            for artists in artistArr {
+              guard let artistID = artists["id"] as? String,
+                let imageArray = artists["images"] as? [[String: Any]],
+                let image = imageArray[1]["url"] as? String,
+                let artistName = artists["name"] as? String else {return nil}
+               
+                let fullArtistInfo = Artists(artistID: artistID, image: image, artistName: artistName)
+                artistInfo.append(fullArtistInfo)
+            }
+}
+        return artistInfo
+}
+}
